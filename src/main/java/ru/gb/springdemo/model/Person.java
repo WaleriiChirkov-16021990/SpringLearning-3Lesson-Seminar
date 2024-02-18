bc3f70459efb34dd0234047d16f176a2ef41dee4
WaleriiChirkov-16021990 <zx84058405@live.com>
2024-02-18 16:56:12 +0300

    пробы тестирования пробуем сначала

--- src/main/java/ru/gb/springdemo/model/Person.java
+++ src/main/java/ru/gb/springdemo/model/Person.java
@@ -1,7 +1,6 @@
 package ru.gb.springdemo.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Cascade;
@@ -10,11 +9,10 @@ import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 import java.util.UUID;
-import static org.hibernate.annotations.CascadeType.ALL;
 @Entity(name = "Person")
 @Getter
 @Setter
-@Table(name = "person")
+@Table(name = "Person")
 @NamedQueries({
         @NamedQuery(name = "Person.findDistinctById", query = "select distinct p from Person p where p.id = :id", lockMode = LockModeType.READ),
         @NamedQuery(name = "Person.findByNameIgnoreCaseOrderByNameAsc", query = "select p from Person p where upper(p.name) = upper(:name) order by p.name", lockMode = LockModeType.READ),
@@ -37,7 +35,7 @@ public class Person {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @Fetch(FetchMode.SELECT)
-    @Cascade(ALL)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
     @JoinTable(name = "persons_roles",
             joinColumns = @JoinColumn(name = "person_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
